244180cc43a1e6a61ca140eb47063c96f6b6ac06
Anish <acharyaanish920@gmail.com>
2024-11-11 14:46:30 +0545

    not found page

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -1,8 +1,11 @@
+import Heading from "../ui/Heading";
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="relative  flex lg:py-80 md:pt-52 pt-32 justify-center items-center w-full    ">
+      <Heading textfirst="404" className="lg:text-7xl  sm:text-5xl  text-4xl"  highlightText="Page Not Found"/>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
